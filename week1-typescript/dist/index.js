f70e93c75d29ecb7244fe9b9f6ac732013a3cdae
goodispotato <potatoisgood@ewhain.net>
2026-09-18 00:43:31 +0900

    chore: 조던 폴더 초기 업로드

---
+++ week1-typescript/dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+const members = [
+    { id: 1, name: "광수", role: "leader", githubId: "gwangsoo" },
+    { id: 2, name: "지수", role: "member" },
+];
+function printMemberInfo(memberId) {
+    const foundMember = members.find((member) => member.id === memberId);
+    if (!foundMember) {
+        return "해당 ID를 가진 회원을 찾을 수 없습니다.";
+    }
+    const displayGithubId = foundMember.githubId ?? "등록되지 않음";
+    return foundMember.name + "님의 역할은 " + foundMember.role + "이고 깃허브 아이디는 " + displayGithubId;
+}
+console.log(printMemberInfo(1));
+console.log(printMemberInfo(2));
+console.log(printMemberInfo(999));
