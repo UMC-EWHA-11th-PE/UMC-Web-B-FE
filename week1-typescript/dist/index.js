5a03f98e26fa4c6a9777498cd4b7393410dbf9de
kimhyeonglyun <ryunn0228@gmail.com>
2026-09-20 22:42:08 +0900

    feat: 스터디 회원 관리

---
+++ week1-typescript/dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+const members = [
+    { id: 1, name: "광수", isLeader: true, githubId: "gwangsoo" },
+    { id: 2, name: "지수", isLeader: false },
+];
+function getMemberInfo(memberId) {
+    const foundMember = members.find((member) => member.id === memberId);
+    if (foundMember) {
+        const githubId = foundMember.githubId ?? "등록되지 않음";
+        return (foundMember.name + " / GitHub: " + githubId);
+    }
+    else {
+        return "존재하지 않는 회원입니다.";
+    }
+}
+console.log(getMemberInfo(1));
+console.log(getMemberInfo(2));
+console.log(getMemberInfo(999));
